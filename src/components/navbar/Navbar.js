5a8961659aad1f468518e60c9a7572a3afc24ce7
siccoo <obichilaka@gmail.com>
2021-06-03 23:44:24 +0100

    updates on Navbar component

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Avatar from "../../images/users.svg";
+
+const Navbar = () => {
+    return (
+        <nav>
+            
+        </nav>
+    )
+}
+
+export default Navbar
