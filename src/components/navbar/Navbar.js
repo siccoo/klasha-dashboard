9cf3a6d922d0cad50933b5efcc11e2f4dff680d8
siccoo <obichilaka@gmail.com>
2021-06-04 00:03:13 +0100

    updates on Navbar component

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import Avatar from "../../images/users.svg";
+import Notification from "../../images/notification.svg";
 
-const Navbar = () => {
+const Navbar = ({ sidebarOpen, openSidebar }) => {
     return (
-        <nav>
-            
+        <nav className="navbar">
+            <div className="nav__icon" onClick={() => openSidebar()}>
+                <i className="fa fa-bars"></i>
+            </div>
+            <div className="navbar__left">
+                <p>Balances</p>
+                <span>Today, 19th October 2020</span>
+            </div>
+            <div className="navbar__right">
+                <a>
+                    <img />
+                </a>
+            </div>
         </nav>
     )
 }
